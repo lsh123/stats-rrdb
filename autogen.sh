b9a2970b5542bf17a38fd1e1ac060e2f93dbbd62
Aleksey Sanin <aleksey@localhost.localdomain>
2013-06-02 22:16:41 -0700

    adding basic autoconf project

---
+++ autogen.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=. 
+
+THEDIR=`pwd`
+cd $srcdir
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed."
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed."
+	DIE=1
+}
+
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoheader installed."
+	DIE=1
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed."
+	DIE=1
+}
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed."
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test -f src/stats-rrdb.cpp  || {
+	echo "You must run this script in the top-level directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+echo "Running libtoolize..."
+libtoolize --copy --force
+echo "Running aclocal..."
+aclocal --force -I m4
+echo "Running autoheader..."
+autoheader --force
+echo "Running autoconf..."
+autoconf --force
+echo "Running automake..."
+automake --gnu --add-missing
+echo "Running autoconf..."
+autoconf
+
+cd $THEDIR
+
+if test x$OBJ_DIR != x; then
+    mkdir -p "$OBJ_DIR"
+    cd "$OBJ_DIR"
+fi
+
+conf_flags="--enable-maintainer-mode" #--enable-iso-c
+echo Running configure $conf_flags "$@" ...
+$srcdir/configure $conf_flags "$@"
+
+echo 
+echo "Now type 'make'."
