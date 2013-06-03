5308afd07924829193c2a5ad7ce80f59e398747f
Aleksey Sanin <aleksey@aleksey.com>
2013-06-02 22:12:51 -0700

    add basic files

---
+++ src/stats-rrdb.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : stats-rrdb.cpp
+ Author      : Aleksey Sanin
+ Version     :
+ Copyright   : Copyright 2013, Aleksey Sanin. All Rights Reserved.
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Test" << endl; /* prints Test */
+	return 0;
+}
