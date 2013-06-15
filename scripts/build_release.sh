b4a06af1a721487913640667d8347d358c50a5b4
Aleksey Sanin <aleksey@aleksey.com>
2013-06-14 19:23:03 -0700

    tweak versions

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 NAME=stats-rrdb
-BUILD_ROOT=/tmp/$NAME.`date`
+BUILD_ROOT=/tmp/$NAME.`date  +"%Y-%m-%d.%H:%m:%S"`
 GIT_URL="https://github.com/lsh123/$NAME.git"
-PWD=`pwd`
+CUR_PWD=`pwd`
+echo "$CUR_PWD"
 
 echo "=== Starting build in '$BUILD_ROOT'"
 mkdir -p "$BUILD_ROOT"
@@ -17,9 +18,9 @@ find . -name ".git" | xargs rm -rf
 echo "=== Configure"
 ./autogen.sh --prefix=/usr
 make dist
-cp $NAME*.tar.gz "$PWD"
+cp $NAME*.tar.gz "$CUR_PWD"
 
 echo "=== Cleanup"
-cd "$PWD"
+cd "$CUR_PWD"
 rm -rf "$BUILD_ROOT"
 
