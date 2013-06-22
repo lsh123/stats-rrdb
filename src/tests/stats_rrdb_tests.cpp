a28c1f6c964a03db13b34aaeb3a02f0ab1d7026d
Aleksey Sanin <aleksey@aleksey.com>
2013-06-21 18:06:22 -0700

    start separate test binary; separate core into a lib

---
+++ src/tests/stats_rrdb_tests.cpp
@@ -0,0 +1,66 @@
+/*
+ ============================================================================
+ Name        : stats-rrdb.cpp
+ Author      : Aleksey Sanin
+ Version     :
+ Copyright   : Copyright 2013, Aleksey Sanin. All Rights Reserved.
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <signal.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include "log.h"
+#include "config.h"
+#include "exception.h"
+#include "server/server.h"
+
+int main(int argc, char ** argv)
+{
+
+  try {
+    // init
+    srand(time(NULL));
+
+    // load config
+    boost::shared_ptr<config> cfg(new config());
+    if(!cfg->init(argc, argv)) {
+        return (0);
+    }
+
+
+    // main server
+    boost::shared_ptr<server> main_server(new server());
+    main_server->initialize(cfg);
+
+    if(cfg->has("test")) {
+        main_server->test(cfg->get<std::string>("test"));
+        return(0);
+    }
+    if(cfg->has("daemon")) {
+        main_server->daemonize(cfg->get<std::string>("daemon"));
+    }
+    if(cfg->has("server.user")) {
+        main_server->setuid_user(cfg->get<std::string>("server.user"));
+    }
+    main_server->run();
+
+  } catch (const std::exception & e) {
+    LOG(log::LEVEL_CRITICAL,  "EXCEPTION: %s", e.what());
+
+    std::cerr << "EXCEPTION: " << e.what() << std::endl;
+    return(1);
+  } catch (...) {
+    LOG(log::LEVEL_CRITICAL,  "EXCEPTION: Unknown exception");
+
+    std::cerr << "EXCEPTION: Unknown Exception" << std::endl;
+    throw;
+  }
+
+  return(0);
+}
